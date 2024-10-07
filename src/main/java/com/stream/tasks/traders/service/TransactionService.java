507a48f66d49a164989097c6099f8bb8e84c3784
alexandr <alexandr.cassa@gmail.com>
2024-10-07 10:48:22 +0300

    User and Transaction Service Implementation
    Transaction Service Implementation
    
    Minor refactor

--- src/main/java/com/stream/tasks/traders/service/TransactionService.java
+++ src/main/java/com/stream/tasks/traders/service/TransactionService.java
@@ -13,46 +13,75 @@ public class TransactionService {
 
     //Find all transactions in the year 2011 and sort them by value, small to high. Lvl: Easy
     public List<Transaction> transactionsBySortedValue(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .filter(transaction -> transaction.getYear() == 2011)
+                .sorted(Comparator.comparing(Transaction::getYear))
+                .collect(Collectors.toList());
     }
 
     //What are all the unique cities where the traders work? Lvl: Easy
     public List<String> allUniqueCities(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     //Find all traders from Cambridge and sort them by name. Lvl: Easy
     public List<Trader> allTradersFromCambridge(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .map(Transaction::getTrader)
+                .distinct()
+                .filter(trader -> isTraderFromCity(trader, "Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
     }
 
     //Return a list of all traders names sorted alphabetically. Lvl: Easy
     public List<String> namesOfTradersSortedAlphabetically(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getName)
+                .distinct()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
     }
 
     //Return a string of all traders names sorted alphabetically. Lvl: Medium but not obvious. Tip: See Collectors that can working with Stream.
     public String namesOfTradersSortedAlphabeticallyAsOneString(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return namesOfTradersSortedAlphabetically(transactionList).stream()
+                .collect(Collectors.joining(" "));
     }
 
     //Are any traders based in Milan? Lvl: Medium
     public boolean areThereTradersFromMilan(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .map(Transaction::getTrader)
+                .anyMatch(trader -> isTraderFromCity(trader, "Milan"));
     }
 
     //Print the values of all transactions from the traders living in Cambridge. Lvl: Easy
     public List<Integer> valuesOfCambridgeTransactions(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .filter(transaction -> isTraderFromCity(transaction.getTrader(), "Cambridge"))
+                .map(Transaction::getValue)
+                .collect(Collectors.toList());
     }
 
     //What's the highest value of all the transactions? Lvl: Medium
     public OptionalInt highestValueOfAllTransactions(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .mapToInt(Transaction::getValue)
+                .max();
     }
 
     //Find the transaction with the smallest value. Lvl: Medium
     public Optional<Transaction> transactionWithSmallestValue(List<Transaction> transactionList) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return transactionList.stream()
+                .min(Comparator.comparing(Transaction::getValue));
+    }
+
+    private static boolean isTraderFromCity(Trader trader, String city){
+        return trader.getCity().equals(city);
     }
 }
--- src/main/java/com/stream/tasks/users/service/UserService.java
+++ src/main/java/com/stream/tasks/users/service/UserService.java
@@ -2,6 +2,7 @@ package com.stream.tasks.users.service;
 
 import com.stream.tasks.users.models.Privilege;
 import com.stream.tasks.users.models.User;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -16,22 +17,40 @@ public class UserService {
 
     //Should return a list of first names sorted in reverse order. Lvl: Medium Tip: See Comparator
     public List<String> getFirstNamesReverseSorted(List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .map(User::getFirstName)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    private static int compareByAgeDescAndNameAsc(User u1, User u2){
+        int result = u2.getAge() - u1.getAge();
+        if(result == 0){
+            result = u1.getFirstName().compareTo(u2.getFirstName());
+        }
+        return result;
     }
 
     //Should sort given list of user by age descending and name ascending. Lvl: Medium
     public List<User> sortByAgeDescAndNameAsc(final List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .sorted(UserService::compareByAgeDescAndNameAsc)
+                .collect(Collectors.toList());
     }
 
     //Should return distinct list of privileges. Lvl: Easy - Medium
     public List<Privilege> getAllDistinctPrivileges(final List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .flatMap(user -> user.getPrivileges().stream())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     //Should return any user whose age greater than given. Lvl: Medium.
     public Optional<User> getUpdateUserWithAgeHigherThan(final List<User> users, final int age) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .filter(user -> user.getAge() > age)
+                .findAny();
     }
 
     /*Should return a map where Key is count of privileges Value is List<User> with this count of privileges. Lvl: Thought, Need to think :)
@@ -39,39 +58,68 @@ public class UserService {
                  3 - UserTwo(CREATE, UPDATE, DELETE)
     */
     public Map<Integer, List<User>> groupByCountOfPrivileges(final List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .collect(Collectors.groupingBy(
+                        user -> user.getPrivileges().size(),
+                        Collectors.toList()
+                ));
     }
 
     //Should return average age for all users. Lvl: Medium. Tip: Check Primitive Streams
     public double getAverageAgeForUsers(final List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .mapToInt(User::getAge)
+                .average()
+                .orElse(-1);
     }
 
     //Should return most frequent last name. Lvl: Hard
     public Optional<String> getMostFrequentLastName(final List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .collect(Collectors.groupingBy(
+                        User::getLastName,
+                        Collectors.counting()
+                )).entrySet().stream()
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getValue,
+                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())
+                )).entrySet().stream()
+                .max(Map.Entry.comparingByKey())
+                .filter(entry -> entry.getValue().size() == 1)
+                .map(entry -> entry.getValue().get(0));
     }
 
     /*Should apply predicates(conditions) to all Users. Lvl: Hard. Tip: Don't forget that each Predicate actually has its method that calls test(T).
     Apply it to all users.*/
     public List<User> filterBy(final List<User> users, final Predicate<User>... predicates) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .filter(Arrays.stream(predicates).reduce(x -> true, Predicate::and))
+                .collect(Collectors.toList());
     }
 
     /*Return string with applied mapFun and Collected with delimiter.
     Lvl: Medium-Interesting. Tip: Remember namesOfTradersSortedAlphabeticallyAsOneString :D.*/
     public String convertTo(final List<User> users, final String delimiter, final Function<User, String> mapFun) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .map(mapFun)
+                .collect(Collectors.joining(delimiter));
     }
 
 
     // Should return map where key is Privilege, value is List of User that have it. Lvl: Pretty Hard. The most hard task here.
     public Map<Privilege, List<User>> groupByPrivileges(List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .flatMap(user -> user.getPrivileges().stream()
+                    .map(privilege -> Pair.of(privilege, user)))
+                .collect(Collectors.groupingBy(
+                        Pair::getKey,
+                        Collectors.mapping(Pair::getValue, Collectors.toList())
+                ));
     }
 
     //Should return map where key is LastName, value Count of this LastName. Lvl: Medium
     public Map<String, Long> getNumberOfLastNames(final List<User> users) {
-        throw new UnsupportedOperationException("Resolve me :3");
+        return users.stream()
+                .collect(Collectors.groupingBy(User::getLastName, Collectors.counting()));
     }
 }
